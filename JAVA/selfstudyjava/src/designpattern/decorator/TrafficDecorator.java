f670dd89dc1b2d48d5b976c4fc6bc0f810b1b602
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-03-19 21:15:11 +0900

    데코레이터 패턴

---
+++ JAVA/selfstudyjava/src/designpattern/decorator/TrafficDecorator.java
@@ -0,0 +1,18 @@
+package designpattern.decorator;
+
+public class TrafficDecorator extends DisplayDecorator {
+    //기존 표시 클래스의 설정
+    public TrafficDecorator(Display decoratedDisplay) {
+        super(decoratedDisplay);
+    }
+
+    @Override
+    public void draw() {
+        super.draw(); //설정된 기존 표시 기능을 수행
+        drawTraffic();
+    }
+
+    private void drawTraffic() {
+        System.out.println("\t교통량 표시");
+    }
+}
