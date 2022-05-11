8b1f7cca6a86d0aa56aa2aa06818d026a7aad932
hansu <71827641+HanSu-yeon@users.noreply.github.com>
2022-05-12 00:09:21 +0900

    list2

--- python/list2.py
+++ python/list2.py
@@ -17,5 +17,28 @@ for x in a:
     print(x, end=' ')
 print()
 
+#enumerate : 인덱스 번호와 컬렉션의 원소를 tuple형태로 반환. for문과 함께 자주 사용
+#ex) (0, 23) (1, 12) ... (4, 19)
 for x in enumerate(a):
   print(x)
+  
+for x in enumerate(a):
+  print(x[0],x[1])
+print()
+
+
+for index, value in enumerate(a):
+  print(index, value)
+print()
+
+#all: 모두 True여야 True 리턴
+if all(60>x for x in a):
+  print("yes")
+else:
+  print("no")
+
+#any: 하나라도 True인게 있으면 True리턴
+if any(10>x for x in a):
+  print("YES")
+else:
+  print("NO")
